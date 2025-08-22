980faa59967cf3709b70e528149e778b250e5b5f
Salman Shahid <salmanshahid0333@gmail.com>
2025-08-21 23:13:22 -0700

    authentication middleware create

---
+++ server/middleware/protect.js
@@ -0,0 +1,16 @@
+// middlewares/authMiddleware.js
+import jwt from "jsonwebtoken";
+import User from "../models/userModal.js";
+
+export const authentication = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) return res.status(401).json({ msg: "No token, not authorized" });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password"); 
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: "Token invalid" });
+  }
+};
