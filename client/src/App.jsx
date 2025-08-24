4c9689d92ed054e614f9f42ed68406b193438fde
Salman Shahid <salmanshahid0333@gmail.com>
2025-08-24 01:27:45 -0700

    protected route wrapped

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,29 @@
-import './App.css'
-import ChatPage from './pages/ChatPage'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ChatPage from "./pages/ChatPage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <>
-      <ChatPage />
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Private Route */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+               <ChatPage />
+           </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
