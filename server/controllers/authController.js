9a0871df4a37616007470673073b30b4d33130ab
Salman Shahid <salmanshahid0333@gmail.com>
2025-08-26 18:10:51 -0700

    signup controller create

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,4 @@
-import User from "../models/userModal.js"
+import User from "../models/userModel.js"
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv";
@@ -30,36 +30,6 @@ export const signup = async (req, res) => {
     await newUser.save();
 
     res.status(201).json({ msg: "User registered successfully", user: newUser });
-  } catch (error) {
-    res.status(500).json({ msg: error.message });
-  }
-};
-
-//login
-
-export const loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ msg: "User not found" });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
-
-    // generate token
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
-    });
-
-    res.json({
-      msg: "Login successful",
-      token,
-      user: {
-        id: user._id, username: user.username, email: user.email, bio: user.bio || "",
-        profilePic: user.profilePic || "" ,
-      },
-    });
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
