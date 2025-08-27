58a217fd1d4be7e53f75378a4323f6c166dcb10d
Salman Shahid <salmanshahid0333@gmail.com>
2025-08-26 18:11:40 -0700

    login controller create

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -34,3 +34,33 @@ export const signup = async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 };
+
+//login
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+    // generate token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.json({
+      msg: "Login successful",
+      token,
+      user: {
+        id: user._id, username: user.username, email: user.email, bio: user.bio || "",
+        profilePic: user.profilePic || "" ,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
