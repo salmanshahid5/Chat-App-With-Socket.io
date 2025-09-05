3653e077e0f121d8e821b027c0055f04863628c6
Salman Shahid <salmanshahid0333@gmail.com>
2025-09-04 21:53:10 -0700

    get friends suggustion route create

---
+++ server/routes/userRoute.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { acceptRequest, cancelFriendRequest, deleteFriendRequest, getFriendRequests, getFriends, getFriendSuggestions, sendFriendRequest, updateProfile } from "../controllers/userController.js";
+import { authentication } from "../middleware/protect.js";
+
+const router = express.Router();
+router.get('/friend-suggestions', authentication, getFriendSuggestions)
+
+
+export default router;
