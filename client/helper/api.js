00cb9d140394611476a0ff52927318c9c10575b7
Salman Shahid <salmanshahid0333@gmail.com>
2025-10-01 22:20:01 -0700

    uninstall pakage emogi mart

--- client/helper/api.js
+++ client/helper/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  // baseURL: "http://localhost:5000/api", 
+  baseURL: "https://chat-app-with-socket-io-ten.vercel.app/api", 
   withCredentials: true, 
   headers: {
     "Content-Type": "application/json", 
--- client/helper/socket.js
+++ client/helper/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://chat-app-with-socket-io-ten.vercel.app", {
   withCredentials: true,
   transports: ["websocket"],
 });
